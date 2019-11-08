e8282cabb95d76329084a35dd94a24f4a3e71550
Andi Rustianto <rustiantoandi17@gmail.com>
2019-11-08 22:34:55 +0700

    styling card

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({dataAlbum}) => {
+
+  const {title,artist,thumbnail_image,image} = dataAlbum;
+  const {thumbnailStyle,headerContentStyle, thumbnailContainerStyle, headerTextStyle, imageStyle} = styles;
 
   return (
     <Card>
       <CardSection>
-        <Text> {props.dataAlbum.title} </Text>
+        <View style={thumbnailContainerStyle}>
+          <Image
+            style={thumbnailStyle}
+            source={{uri: thumbnail_image}}
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}> {title} </Text>
+          <Text> {artist} </Text>
+        </View>
       </CardSection>
-      
+      <CardSection>
+        <Image
+          style={imageStyle}
+          source={{uri: image}}
+        />
+      </CardSection>
     </Card>
   );
-  
+}
+
+const styles = {
+  thumbnailContainerStyle : {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  headerTextStyle:{
+    fontSize: 18,
+    
+  },
+  headerContentStyle : {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  thumbnailStyle : {
+    height : 50,
+    width: 50,
+  },
+  imageStyle : {
+    height: 300,
+    width: null,
+    flex: 1,
+  }
 }
 
 
