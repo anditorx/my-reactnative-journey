024488cc7a78e6933a72df9b21cfc65271a051fa
Andi Rustianto <rustiantoandi17@gmail.com>
2019-11-10 02:39:45 +0700

    boilerplate redux

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducers from './reducers'
 
 const App = () => {
 
   return(
-    <View></View>
+    <Provider store={createStore(reducers)}>
+      <View/>
+    </Provider>
   );
 
 }
