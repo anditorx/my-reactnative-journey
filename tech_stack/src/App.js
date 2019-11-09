02206cada3c51524ba3bf69707ff74959b95bb9b
Andi Rustianto <rustiantoandi17@gmail.com>
2019-11-10 02:45:30 +0700

    create common component

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -3,11 +3,13 @@ import { Text, View } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducers from './reducers'
+import { Header } from './components/common'
 
 const App = () => {
 
   return(
     <Provider store={createStore(reducers)}>
+      <Header headerText={"Tech Stack"} />
       <View/>
     </Provider>
   );
