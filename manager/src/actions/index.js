53d4efb59c6e3e801c25ac08e52b3d2a3d925a13
Andi Rustianto <rustiantoandi17@gmail.com>
2019-11-17 03:45:21 +0700

    install redux-thunk

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 import { 
   EMAIL_CHANGED, 
   PASSWORD_CHANGED 
@@ -16,4 +18,10 @@ export const passwordChanged = (text) => {
     type: PASSWORD_CHANGED,
     payload: text
   }
+}
+
+export const loginUser = ({ email, password }) => {
+  firebase.auth().signInWithEmailAndPassword(email,password)
+  .then(user => console.log(user));
+  )
 }
