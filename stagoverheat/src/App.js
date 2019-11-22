48530ad772757a04735e75988a74a173be995a7e
Air Dev <airdev@Airs-MacBook-Air.local>
2019-11-22 16:08:15 +0700

    create screens

---
+++ stagoverheat/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import {Actions, Scene, Router, Switch, Modal } from 'react-native-router-flux';
+import About from './components/screens/About';
+
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+
+
+export default App;
---
+++ stagoverheat/src/components/screens/Question.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class Question extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Question </Text>
+      </View>
+    )
+  }
+}
