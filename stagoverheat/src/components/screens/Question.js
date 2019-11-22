6065dc29bd632ab3492587afddaf521040a189e2
andiankas <rustiantoandi17@gmail.com>
2019-11-22 17:19:30 +0700

    flatlist

--- stagoverheat/src/components/screens/Question.js
+++ stagoverheat/src/components/screens/Question.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, FlatList } from 'react-native'
+
+const QuestionList = [
+  {title:"First Question", author: "Yahya"},
+  {title:"Second Question", author: "Diki"},
+  {title:"Third Question", author: "Budi"},
+  {title:"Fourth Question", author: "Jimmy"},
+  {title:"5 Question", author: "Doni"},
+];
+
+function Item ({title, author}) {
+  return (
+    <Text>{title} - {author}</Text>
+  );
+}
 
 export default class Question extends Component {
+
   render() {
     return (
       <View>
-        <Text> Question </Text>
+        <FlatList 
+          data={QuestionList}
+          renderItem={({item}) => <Item title={item.title} author={item.author} />}
+          keyExtractor={item => item.title}
+        />
       </View>
     )
   }
