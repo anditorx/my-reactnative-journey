7c3c173d34ed4ed06fa7d3bb6e155682532714b5
andiankas <rustiantoandi17@gmail.com>
2019-11-22 16:44:01 +0700

    create router on app

--- stagoverheat/src/App.js
+++ stagoverheat/src/App.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 import {Actions, Scene, Router, Switch, Modal } from 'react-native-router-flux';
 import About from './components/screens/About';
+import Question from './components/screens/Question';
 
+
+class TabIcon extends Component {
+  render(){
+    const title = this.props.title;
+    return(
+      <Text>{title}</Text>
+    );
+  };
+}
 
 class App extends Component {
+
+  componentWillMount() {
+    this.scenes = Actions.create(
+      <Scene key="root" tabs={true} hideTabBar={true}>
+        <Scene key="tabbar" tabs={true} tabBarStyle={{backgroundColor:'#f7f7f7'}} >
+          <Scene key="Questions" component={Question} title="Questions" icon={TabIcon} hideNavBar={true} />
+          <Scene key="About" component={About} title="About" icon={TabIcon} hideNavBar={true} />
+        </Scene>
+      </Scene>
+    );
+  }
+
   render() {
-    return (
-      <View>
-        <Text> App </Text>
-      </View>
-    )
+    return <Router scenes={this.scenes} />
   }
 }
 
