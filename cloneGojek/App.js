18feb3958b4dde6566d2b7652fbbf2c3df0c1206
andiankas <rustiantoandi17@gmail.com>
2020-02-08 16:59:43 +0700

    search bar

--- cloneGojek/App.js
+++ cloneGojek/App.js
@@ -7,6 +7,7 @@ import {
   View,
   Text,
   StatusBar,
+  TextInput,
   Image
 } from 'react-native';
 
@@ -15,9 +16,17 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         {/* container */}
-        <View style={{flex: 1,backgroundColor: 'pink'}}>
-          <Text>Test</Text>
-
+        <View style={{flex: 1,backgroundColor: 'white'}}>
+          {/* search bar */}
+          <View style={{marginHorizontal: 17, flexDirection: 'row', paddingTop: 15}}>
+            <View style={{position: 'relative', flex: 1}}>
+              <TextInput placeholder="What do you want to eat?" style={{borderWidth: 1,borderColor: '#E8E8E8', borderRadius: 25, height: 40, fontSize: 13, paddingLeft: 45, paddingRight: 20, backgroundColor: 'white', marginRight: 18}} />
+              <Image source={require('./assets/icon/search.png')} style={{position: 'absolute', top: 7, left: 10}} />
+            </View>
+            <View style={{width: 35, alignItems: 'center', justifyContent: 'center'}}>
+              <Image source={require('./assets/icon/promo.png')} />
+            </View>
+          </View>
         </View>
         {/* bottom navigation */}
         <View style={{height: 75, flexDirection:'row', backgroundColor: 'white'}}>
