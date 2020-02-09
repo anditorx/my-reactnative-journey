ec06ba3bc06c95bb1e4335dee3fad9ae7628d39a
andiankas <rustiantoandi17@gmail.com>
2020-02-09 20:27:19 +0700

    internal information section

--- cloneGojek/App.js
+++ cloneGojek/App.js
@@ -128,6 +128,26 @@ export default class App extends Component {
               </TouchableOpacity>
             </View>
           </View>
+          {/* internal information section */}
+          <View style={{padding: 16, paddingBottom: 0}}>
+            <View style={{height: 15, width: 60, marginLeft: -4}} >
+              <Image source={require('./assets/logo/gojek.png')} style={{height: undefined, width: undefined, resizeMode: 'contain', flex: 1}} />
+            </View>
+            <Text style={{fontSize: 17, fontWeight: 'bold', marginTop: 15, marginBottom: 20}}>Complete your profile</Text>
+            <View style={{flexDirection: "row", marginBottom: 16}}>
+              <View>
+                <Image source={require('./assets/dummy/facebook-connect.png')} />
+              </View>
+              <View style={{marginLeft: 16, flex: 1}}>
+                <Text style={{fontSize: 15, fontWeight: 'bold', color: '#4A4A4A'}}>Connect with Facebook</Text>
+                <Text style={{fontSize: 14, color: '#4A4A4A', width: '60%'}}>Login faster without verification code</Text>
+              </View>
+            </View>
+            <TouchableOpacity style={{backgroundColor: '#61A756', paddingHorizontal: 12, paddingVertical: 11, alignSelf: 'flex-end', borderRadius: 4}}>
+              <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white', textAlign: 'center'}}>CONNECT</Text>
+            </TouchableOpacity>
+            <View style={{ borderBottomColor: '#E8E9ED', borderBottomWidth: 1, paddingBottom: 16}}></View>
+          </View>
           {/* gofood banner section */}
           <View style={{padding: 16}}>
             <View style={{position: 'relative'}}>
