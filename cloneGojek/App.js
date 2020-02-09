be4a9316d6283e379097050c138cd69ac2d932f2
andiankas <rustiantoandi17@gmail.com>
2020-02-09 16:42:55 +0700

    banner section

--- cloneGojek/App.js
+++ cloneGojek/App.js
@@ -128,6 +128,26 @@ export default class App extends Component {
               </TouchableOpacity>
             </View>
           </View>
+          {/* gofood banner section */}
+          <View style={{padding: 16}}>
+            <View style={{position: 'relative'}}>
+              <Image source={require('./assets/dummy/food-banner.jpg')} style={{height: 200, width: '100%',borderRadius: 6}} />
+              <View style={{width: '100%',height: '100%', position: 'absolute', backgroundColor: 'black', opacity: 0.2, borderRadius: 6 }}></View>
+              <View style={{height: 15, width: 55, position: 'absolute', top: 16, left: 16}} >
+                <Image source={require('./assets/logo/white.png')} style={{height: undefined, width: undefined, resizeMode: 'contain', flex: 1}} />
+              </View>
+              <View style={{position: 'absolute', bottom: 0, left: 0, width: '100%', flexDirection: 'row', alignItems: 'center', paddingHorizontal: 16, paddingBottom: 16}}>
+                <View style={{flex: 1, paddingLeft: 12}}>
+                  <Text style={{fontSize: 18, fontWeight: 'bold', color: 'white', marginBottom: 5}}>Free GO-FOOD Voucher</Text>
+                  <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white'}}>Quick, before they run out!</Text>
+                </View>
+                <TouchableOpacity style={{backgroundColor: '#61A756', paddingHorizontal: 12, paddingVertical: 11, alignSelf: 'flex-end', borderRadius: 4}}>
+                  <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white', textAlign: 'center', alignSelf: 'stretch'}}>GET VOUCHER</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={{ borderBottomColor: '#E8E9ED', borderBottomWidth: 1, paddingBottom: 16}}></View>
+          </View>
         </ScrollView>
         {/* bottom navigation */}
         <View style={{height: 75, flexDirection:'row', backgroundColor: 'white'}}>
