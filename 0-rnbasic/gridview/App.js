9c168392043084feb6514ea972cede92cf0a1652
andiankas <rustiantoandi17@gmail.com>
2020-03-04 17:42:57 +0700

    testing cocoapods

---
+++ 0-rnbasic/gridview/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import {StyleSheet, Text,View,FlatList} from 'react-native'
+
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        
+      </>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default App
