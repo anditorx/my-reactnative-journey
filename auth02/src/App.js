bf85d52469a4f7484b6f667d0ece0e0340148f47
andiankas <rustiantoandi17@gmail.com>
2020-03-16 11:55:21 +0700

    firebase config

--- auth02/src/App.js
+++ auth02/src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View,SafeAreaView, } from 'react-native'
+import firebase from 'firebase';
+import {Header} from './components/common';
 
 export default class App extends Component {
+
+	componentDidMount() {
+		firebase.initializeApp({
+			apiKey: "AIzaSyCDRBw3KyjO3R5art-FE4TESYZaKSNTkIw",
+			authDomain: "auth-95672.firebaseapp.com",
+			databaseURL: "https://auth-95672.firebaseio.com",
+			projectId: "auth-95672",
+			storageBucket: "auth-95672.appspot.com",
+			messagingSenderId: "396628262445",
+			appId: "1:396628262445:web:e15d9b4e2a6cdd510f82a4"
+		});
+	}
+
 	render() {
 		return (
-			<View>
+			<SafeAreaView>
+				<Header headerText = {"Authentication"} />
 				<Text> textInComponent </Text>
-			</View>
+			</SafeAreaView>
 		)
 	}
 }
