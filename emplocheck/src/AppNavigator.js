b4f850a4d7039180e3bff54fa45ba9d869d149fc
andiankas <rustiantoandi17@gmail.com>
2020-03-18 15:34:33 +0700

    picker day

--- emplocheck/src/AppNavigator.js
+++ emplocheck/src/AppNavigator.js
@@ -2,27 +2,28 @@ import React from 'react';
 import { View, Text } from 'react-native';
 // import { createAppContainer } from 'react-navigation';
 // import { createStackNavigator } from 'react-navigation-stack';
-import {Scene, Router} from 'react-native-router-flux'
+import {Scene, Router, Actions} from 'react-native-router-flux'
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList'
+import EmployeeCreate from './components/EmployeeCreate'
 
-// const AppNavigator = createStackNavigator(
-//   {
-// 		Login: LoginForm,
-// 		List: EmployeeList,
-// 	},
-// 	{
-//     initialRouteName: 'Login',
-//   }
-// );
-
-// export default createAppContainer(AppNavigator);
 const AppNavigator = () =>{
 	return (
 		<Router>
-			<Scene key="root">
-				<Scene key="login" component={LoginForm} title="Please Login"/>
-				<Scene key="employeeList" component={EmployeeList} title="Employee List"/>
+			<Scene key="root" hideNavBar>
+				<Scene key="auth" >
+					<Scene key="login" component={LoginForm} title="Please Login"/>
+				</Scene>
+				<Scene key="main">
+					<Scene 
+						rightTitle="Add"
+						onRight={() => Actions.employeeCreate()}
+						key="employeeList" 
+						component={EmployeeList} 
+						title="Employee List"
+					/>
+					<Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee"/>
+				</Scene>
 			</Scene>
 		</Router>
 	);
