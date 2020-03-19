777106fa51eeaea8e3e757e27742cd01a611c7ca
andiankas <rustiantoandi17@gmail.com>
2020-03-19 14:29:00 +0700

    initial commit

--- rnn_boilerplate/index.js
+++ rnn_boilerplate/index.js
@@ -1,9 +1,21 @@
-/**
- * @format
- */
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// AppRegistry.registerComponent(appName, () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+
+import { Navigation } from "react-native-navigation";
+import App from "./App";
+
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});
