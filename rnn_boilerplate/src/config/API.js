f8a42f44f327c9bf33ca3d3ac2f8919927d62831
andiankas <rustiantoandi17@gmail.com>
2020-04-15 16:19:01 +0700

    react native geolocation

--- rnn_boilerplate/src/config/API.js
+++ rnn_boilerplate/src/config/API.js
@@ -1,3 +1,4 @@
 export default {
-	base_url: 'http://192.168.1.38/xlearn/ci-restserver-master/api/'
+	// base_url: 'http://192.168.1.38/xlearn/ci-restserver-master/api/'
+	base_url: 'http://192.168.43.35/xlearn/ci-restserver-master/api/'
 }
